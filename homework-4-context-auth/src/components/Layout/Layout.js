83b3d66f3f0b122eab5959f570ccc5cbad753816
Дмитрий Глотов <>
2019-08-08 13:59:58 +0300

    fix auth , refactoring layout , realize header & footer components

--- homework-4-context-auth/src/components/Layout/Layout.js
+++ homework-4-context-auth/src/components/Layout/Layout.js
@@ -1,34 +1,43 @@
 import React, { PureComponent } from 'react';
+import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
   render() {
-    const { header, footer } = this.props;
+    const { header, footer, children } = this.props;
+    return (
+      <React.Fragment>
+        {header ? this.renderHeader(header) : null}
 
-    if (!header && !footer) {
-      return (
-        <div>
-          <main />
-        </div>
-      );
-    }
-    if (header) {
-      return (
-        <div>
-          <header className="header" />
-          <main className="main--with-header" />
-        </div>
-      );
-    }
-    if (footer) {
-      return (
-        <div>
-          <footer className="footer" />
-          <main className="main--with-footer" />
-        </div>
-      );
-    }
+        <main
+          className={`main ${header ? 'main--with-header' : ''} ${
+            footer ? 'main--with-footer' : ''
+          }`}
+        >
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {children}
+        </main>
+
+        {footer ? this.renderFooter(footer) : null}
+      </React.Fragment>
+    );
   }
+
+  renderHeader = Header => (
+    <header className="header">
+      <SectionTitle className="header__title">Header</SectionTitle>
+      <div className="header__content">
+        <Header />
+      </div>
+    </header>
+  );
+
+  renderFooter = Footer => (
+    <footer className="footer">
+      <SectionTitle className="header__title">Footer</SectionTitle>
+      <Footer />
+    </footer>
+  );
 }
 
 export default Layout;
--- homework-4-context-auth/src/contexts/Auth/Auth.js
+++ homework-4-context-auth/src/contexts/Auth/Auth.js
@@ -3,11 +3,17 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
-  state = {
-    email: '',
-    authorizeError: false,
-    isAuthorized: false
-  };
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      authorizeError: '',
+      isAuthorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
   authorize(email, password) {
     if (email === 'stu@dent.com' && password === '123') {
       this.setState({ email, isAuthorized: true, authorizeError: '' });
@@ -15,15 +21,21 @@ class AuthProvider extends PureComponent {
     }
     this.setState({ authorizeError: 'Email или пароль введён не верно' });
   }
+
   logout() {
-    this.setState({ isAuthorized: false });
+    this.setState({ isAuthorized: false, email: '' });
   }
+
   getProviderValue() {
-    return this.state;
+    return {
+      ...this.state,
+      authorize: this.authorize,
+      logout: this.logout
+    };
   }
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 
